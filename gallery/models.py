5e0a2f8ccfcb8b4eb1eecadc4d8e7fd2207811c3
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-04 13:34:58 +0300

    does migration for category model

--- gallery/models.py
+++ gallery/models.py
@@ -3,14 +3,14 @@ from django.db import models
 class Locations(models.Model):
     name=models.CharField(max_length=30)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-# class Category(models.Model):
-#     name=models.CharField(max_length=30)
+class Category(models.Model):
+    name=models.CharField(max_length=30)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
 # class Image(models.Model):
 #     name=models.CharField(max_length=30)

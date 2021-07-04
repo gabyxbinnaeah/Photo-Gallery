2c0bbae64816d97220ab59b53f7257aaf939d3d3
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-04 13:22:52 +0300

    craetes category model

--- gallery/models.py
+++ gallery/models.py
@@ -6,9 +6,9 @@ class Locations(models.Model):
     def __str__(self):
         return self.name
 
-# class Category(models.Model):
-#     name=models.CharField(max_length=30)
+class Category(models.Model):
+    name=models.CharField(max_length=30)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 

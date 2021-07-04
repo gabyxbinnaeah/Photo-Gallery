fdc1f1f395fc797fb921ef9e64d89c255c257aa1
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-04 13:32:54 +0300

    creates model location and does initial migrations

--- gallery/models.py
+++ gallery/models.py
@@ -3,12 +3,19 @@ from django.db import models
 class Locations(models.Model):
     name=models.CharField(max_length=30)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-class Category(models.Model):
-    name=models.CharField(max_length=30)
+# class Category(models.Model):
+#     name=models.CharField(max_length=30)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
+
+# class Image(models.Model):
+#     name=models.CharField(max_length=30)
+#     description=models.TextField()
+#     category=models.ForeignKey('Category',on_delete=CASCADE,blank=True)
+#     location=models.ForeignKey('Location',on_delete=CASCADE,blank=True)
+#     image=models.ImageField(upload_to='images/',blank=True)
 

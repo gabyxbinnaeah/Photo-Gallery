820af7bf6a0b3c9e994c1e9fbe60c320691d80b0
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-04 18:38:46 +0300

    creates search view function

--- gallery/views.py
+++ gallery/views.py
@@ -4,3 +4,16 @@ from .models import Image
 def gallery(request):
     photo_contents=Image.found_images()
     return render(request, 'gallery.html',{"photo_contents":photo_contents})
+
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_categories = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-gallery/search.html',{"message":message,"categories": searched_categories})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-gallery/search.html',{"message":message})

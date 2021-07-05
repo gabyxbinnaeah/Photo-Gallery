e7bffdcbf54a90cdee5e9a12c0c3ae8e98e708cd
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-06 00:15:32 +0300

    adds doctstring to methods

--- gallery/models.py
+++ gallery/models.py
@@ -7,12 +7,23 @@ class Location(models.Model):
         return self.name
 
     def save_location(self):
+        '''
+        method that saves location
+        '''
         self.save() 
 
     def delete_location(self):
+        '''
+        method that deletes location
+        '''
         Location.objects.filter(id=self.id).delete()
+
     @classmethod
     def update_location(cls,id,name):
+        '''
+        method that updates location 
+        '''
+
         return cls.objects.filter(id=id).update(name=name)
 
 
@@ -25,12 +36,21 @@ class Category(models.Model):
         return self.name
 
     def save_category(self):
+        '''
+        method that saves category
+        '''
         self.save()  
 
     def delete_category(self):
+        '''
+        method that deletes category
+        '''
         Category.objects.filter(id=self.id).delete()
     @classmethod
     def update_category(cls,id,new):
+        '''
+        method that updates category
+        '''
         return cls.objects.filter(id=id).update(new=new)
 
 
@@ -47,23 +67,38 @@ class Image(models.Model):
         return self.name
 
     def save_images(self):
+        '''
+        method that saves images
+        '''
         self.save()
 
     def delete_image(self):
+        '''
+        method that deletes images
+        '''
         Image.objects.filter(id=self.id).delete()
 
     @classmethod
     def found_images(cls):
+        '''
+        method that queries and return all image Properties
+        '''
         images_list=cls.objects.all()
         return images_list  
 
     @classmethod
     def search_by_category(cls,search_term):
+        '''
+        method that  searches and return image by category
+        '''
         searched_category=cls.objects.filter(category__name__icontains=search_term)
         return searched_category   
 
     @classmethod
     def search_by_location(cls,search_term):
+        '''
+        method that  searches and return image by location
+        '''
         searched_location=cls.objects.filter(location__name__icontains=search_term)
         return  searched_location
          
--- gallery/tests.py
+++ gallery/tests.py
@@ -5,23 +5,38 @@ from .models import Location,Category,Image
 
 class LocationTestClass(TestCase):
     def setUp(self):
+        '''
+        method that creates instance of location 
+        '''
         self.bondo = Location(name = 'bondo')
 
     def test_instance(self):
+        '''
+        method that test if instance of location is generate
+        '''
         self.assertTrue(isinstance(self.bondo, Location))
 
     def test_save_location(self):
+        '''
+        function that test if location is saved t
+        '''
         self.bondo.save_location()
         searched_locations = Location.objects.all()
         self.assertTrue(len(searched_locations ) >0) 
 
     def test_delete_location(self):
+        '''
+        function that test if location can be deleted 
+        '''
         self.bondo.save_location()
         self.bondo.delete_location()
         found_location=Location.objects.all()
         self.assertTrue(len(found_location)==0) 
 
     def test_update_location(self):
+        '''
+        method that test if location can be updated
+        '''
         self.bondo.save_location()
         self.bondo.update_location(self.bondo.id,'Nairobi')
         location_list=Location.objects.all()
@@ -33,23 +48,38 @@ class LocationTestClass(TestCase):
 
 class CategoryTestClass(TestCase):
     def setUp(self):
+        '''
+        method that creates instance of category 
+        '''
         self.large = Category(name = 'large')
 
     def test_instance(self):
+        '''
+        method that test if instance of category is generate
+        '''
         self.assertTrue(isinstance(self.large, Category))  
 
     def test_save_category(self):
+        '''
+        function that test if category is saved 
+        '''
         self.large.save_category()
         searched_category = Category.objects.all()
         self.assertTrue(len(searched_category  ) >0) 
 
     def test_delete_category(self):
+        '''
+        function that test if category can be deleted 
+        '''
         self.large.save_category()
         self.large.delete_category()
         found_category=Category.objects.all()
         self.assertTrue(len(found_category)==0)
 
     def test_update_category(self):
+        '''
+        method that test if category can be updated
+        '''
         self.large.save_category()
         self.large.update_category(self.large.id,'small')
         returned_category_list=Category.objects.all()
@@ -59,17 +89,29 @@ class CategoryTestClass(TestCase):
 
 class ImageTestClass(TestCase):
     def setUp(self):
+        '''
+        method that creates instance of image whenever test is run
+        '''
         self.tech= Image (name='tech',description='network topology')
          
     def test_instance(self):
+        '''
+        method that test if instance of image is created
+        '''
         self.assertTrue(isinstance(self.tech, Image))
 
     def test_save_image(self):
+        '''
+        function that checks if image is saved 
+        '''
         self.tech.save_images()
         searched_image = Image.objects.all()
         self.assertTrue(len(searched_image) >0) 
 
     def test_delete_image(self):
+        '''
+        method that checks if image can be deleted 
+        '''
         self.tech.save_images()
         self.tech.delete_image()
         found_after_delete=Image.objects.all()
